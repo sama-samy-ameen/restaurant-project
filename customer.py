35c8810526601b098a88ad53532def4206fbfd7d
sama-samy-ameen <samaelkholy992@gmail.com>
2026-08-24 17:54:32 +0300

    sill working on error handling in order function

--- customer.py
+++ customer.py
@@ -1,12 +1,13 @@
 class MenuItems:
-    def init(self, item_name, item_id, price, availability=True ):
+    def __init__(self, item_name, item_id, price, availability=True ):
         self.item_name=item_name
         self.item_id=item_id
         self.price=price
         self.availability=availability
+        
 
 class Order:
-    def init(self, table_number, order_id):
+    def __init__(self, table_number=None, order_id=100):
         self.table_number=table_number
         self.order_id=order_id
         self.items=[]
@@ -14,7 +15,6 @@ class Order:
         self.total_price=0
 
 #Global shared menu for testing/demonstration.
-
 global_menu=[
     MenuItems("Pizza", 1, 250),
     MenuItems("Pasta", 2, 210),
@@ -23,13 +23,90 @@ global_menu=[
     MenuItems("Soda", 5, 60),
     MenuItems("Water", 6, 25)]
 
+
 global_orders=[]
 total_revenue=0
-
-class Customer:
-    #customer authorities:"make an order from the menu, buy it, do reviews"
+# import MainFile.py
+class customer(Order):
     def __init__(self):
-        print('         welcome to our restaurant!          \n Choose what you’d like from our menu to place your order')
-        print(global_menu)
+        #choosing items and placing orders
+        print(' welcome to our restaurant,\n','choose the items you want from our menu:')
+        for i in global_menu:
+            print( '    ','(',i.item_id,')',i.item_name,':',i.price,'EGP')
+        super().__init__()
+    def Order(self):
+        try:
 
-print(global_menu)
+            try: 
+                self.amount=int(input('How many items do you want? '))
+                self.order={}
+                if self.amount<=0 or self.amount>len(global_menu)
+            except:
+                print('please enter a valid input')
+            for i in range(self.amount):
+                item=input("what will you order?  ")
+                #for i in global_menu:
+                 #   if item.lower() not in self.item_name.lower():
+                  #      print('please enter a valid option')
+                   #     break
+
+                   # self.amount=int(input('How many items do you want? '))
+                    #for i in range(self.amount):
+                     #   item=input('what will you order? ')
+                      #  try:
+                       #     quant=int(input(f'How much {item} do you want ? '))
+                        #except:
+                         #   print('please enter a valid input')
+                try:
+                    quant=int(input(f'How much {item} do you want ? '))
+                except:
+                    print('please enter a valid input')
+                self.order[item]=int(quant)
+            confirmation=input(f'Are you sure you want to place this order {self.order} ? :')
+            if 'yes' in confirmation.lower():
+                for key in self.order:
+                    self.items.append(key)
+                try:
+                    self.table_number=int(input('Enter your table number: '))
+                    self.order_id+=1
+                except:
+                    print('please enter a valid input')
+                    #break
+
+                for key,value in self.order.items():
+                    if key==self.item_name:
+                        self.total_price += (self.price *value)
+                print('your order has been confirmed successfully!')
+
+
+            elif 'no' in confirmation.lower():
+                print('please place the order again')
+        
+        except:
+            print('sorry, an error occurred , please resubmit your order ')
+    
+        
+
+
+
+        
+
+      #errors:  the total price part   , error handling 
+        
+
+
+
+
+
+        
+    #def Budget(self):
+        pass 
+    #def order_status(self):
+        pass
+    #def add item(self)
+    #def history(self):
+        pass
+    #def review(self):
+       # pass #if low send to the manager
+c1=customer()
+c1.Order()
