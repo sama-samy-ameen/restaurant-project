35eb830b2cfc1d30b8880d1edf2a10ffdf3e8969
sama-samy-ameen <samaelkholy992@gmail.com>
2026-08-24 19:53:04 +0300

    tested order function, it works

--- customer.py
+++ customer.py
@@ -30,60 +30,79 @@ total_revenue=0
 # import MainFile.py
 class customer(Order):
     def __init__(self):
-        #choosing items and placing orders
+        #displaying the menu
         print(' welcome to our restaurant,\n','choose the items you want from our menu:')
         for i in global_menu:
             print( '    ','(',i.item_id,')',i.item_name,':',i.price,'EGP')
         super().__init__()
-    def Order(self):
-        try:
 
-            try: 
+
+    def Order(self):
+        try:  # a general exception handling so that the program doesn't crash
+            try: #taking the the overall amount of items , then taking each one in detail
+
                 self.amount=int(input('How many items do you want? '))
                 self.order={}
-                if self.amount<=0 or self.amount>len(global_menu):
-                    print('out of range, try again')
+                # amount condition
+                while self.amount<=0 or self.amount>len(global_menu):
+                    self.amount=int(input('How many items do you want? '))
+                    
             except:
                 print('please enter a valid number of items')
+            #taking the order in detail and placing it in a dictionary
             for i in range(self.amount):
-                item=input("what will you order?  ")
-                #for i in global_menu:
-                 #   if item.lower() not in self.item_name.lower():
-                  #      print('please enter a valid option')
-                   #     break
+                try:
+                    item=input("what will you order? :")  
+                    #item conditions
+                    valid=False
+                    while not valid:
+                        for i in global_menu:
+                            if item.capitalize()==i.item_name:
+                                valid=True
+                                break
+                            
+                        if valid==False:
+                            print('this item is currently unavailable or maybe you just wrote it incorrectly')
+                            item=input("what will you order? :") 
+                            
 
-                   # self.amount=int(input('How many items do you want? '))
-                    #for i in range(self.amount):
-                     #   item=input('what will you order? ')
-                      #  try:
-                       #     quant=int(input(f'How much {item} do you want ? '))
-                        #except:
-                         #   print('please enter a valid input')
+                    
+
+                except:
+                    print('invalid input')
+                    break
+                
                 try:
                     quant=int(input(f'How much {item} do you want ? '))
                 except:
                     print('please enter a valid quantity')
                 self.order[item]=int(quant)
+
+            # confirming the order 
             confirmation=input(f'Are you sure you want to place this order {self.order} ? :')
             if 'yes' in confirmation.lower():
+                #adding the order of the customer to the list inside the Order class, so that the chef and the managers can have access to it
                 for key in self.order:
                     self.items.append(key)
-                try:
+                try:  #more info of the customer order
                     self.table_number=int(input('Enter your table number: '))
-                    self.order_id+=1
+                    self.order_id+=1  #so that the next order's id becomes different
                 except:
                     print('please enter a valid table number')
-                    #break
-
+                    
+                #calculating the price
                 for key,value in self.order.items():
                         for item in global_menu:
-                             if key.capitalize()==item.item_name:
+                            if key.capitalize()==item.item_name:
+                                print('match')
                                 self.total_price += (item.price *value)
+                            
                 print('your order has been confirmed successfully!')
+                print(self.total_price)
 
 
             elif 'no' in confirmation.lower():
-                print('please place the order again')
+                print('please place your order again')
         
         except:
             print('sorry, an error occurred , please resubmit your order ')
@@ -91,10 +110,9 @@ class customer(Order):
         
 
 
-
         
 
-      #errors:  the total price part   , error handling 
+      #errors:  error handling 
         
 
 
@@ -103,12 +121,12 @@ class customer(Order):
 
         
     #def Budget(self):
-        pass 
+        #pass 
     #def order_status(self):
-        pass
+        #pass
     #def add item(self)
     #def history(self):
-        pass
+        #pass
     #def review(self):
        # pass #if low send to the manager
 c1=customer()
